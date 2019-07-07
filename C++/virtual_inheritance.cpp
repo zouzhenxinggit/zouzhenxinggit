eac89197e98d6d4e80acd80a8e0777702e5d1ebb
zouzhenxing <zouzhenxing.it@foxmail.com>
2019-07-07 19:32:40 +0800

    virtual public order

--- C++/virtual_inheritance.cpp
+++ C++/virtual_inheritance.cpp
@@ -9,17 +9,15 @@ public:
 };
 
 class A: virtual public ALL
-// class A: public ALL
 {
 public:
 	int a;
 };
 
 class B: virtual public ALL
-// class B: public ALL
 {
 public:
-	int a;
+	int b;
 };
 
 class C: public A, public B
@@ -28,21 +26,47 @@ public:
 	int c;
 };
 
+class CC: public C
+{
+public:
+int cc;
+};
 
+class D: public virtual A, public virtual B
+{
+public:
+	int d;
+};
+
+ // class E: public  B, public virtual A
+  class E: public virtual A, public  B
+{
+public:
+	int e;
+};
+
+#include<stdio.h>
 
 int main(int argc, char const *argv[])
 {
-	C c;
-	c.A::a = 10;
-	c.B::a = 20;
+	// E c;
+	// c.e = 1;
+	// c.a = 2;
+	// c.b = 3;
+	// c.all = 4;
 
-	cout << c.A::a << endl;
-	cout << c.B::a << endl;
+	// int *p = (int*)&(c);
 
-	//虚继承
-	c.all = 10;
+	// while(1) {
+	// 	cout << *p << endl;
+	// 	p++;
+	// 	getchar();
+	// }
 
-	cout << sizeof(A) <<endl;
-
+	// cout << sizeof(C) << endl;
+	// cout << sizeof(CC) << endl;
+	// cout << sizeof(D) << endl;
+	cout << sizeof(E) << endl;
 	return 0;
 }
+
