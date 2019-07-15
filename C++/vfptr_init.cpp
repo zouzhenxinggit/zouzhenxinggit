9e768a4cd0b8543f7cd75417ba71426ccdca4b6b
zouzhenxing <zouzhenxing.it@foxmail.com>
2019-07-15 17:51:21 +0800

    virtual fun

--- C++/vfptr_init.cpp
+++ C++/vfptr_init.cpp
@@ -12,9 +12,10 @@ public:
   father (int a)
   {
     this->a = a;
+    printf();
   }
 
-  virtual ~father ()
+  ~father ()
   {
     cout << "~father" << endl;
   }
@@ -36,9 +37,10 @@ public:
   child (int a) : father (a / 2)
   {
     this->a = a;
+    printf();
   }
 
-  virtual ~child () // virtual子类可省略
+  ~child () // virtual子类可省略
   {
     cout << "~child" << endl;
   }
@@ -50,32 +52,13 @@ public:
   }
 };
 
-void stage(father* &p)
+#include <stdio.h>
+
+int main(int argc, char const *argv[])
 {
-  delete p;
-  while (1) {
-    /* code */
-  }
-}
-
-int main(int argc, char const *argv[]) {
-
-  // 1.继承
-  // 2.需函数重写(参数个数，类型相同)
-  // 3.父类指针或引用指向父类对象或子类对象 会有不同的调用方式
-
-  /* code */
-  father f(6);
   child c(10);
 
-  father *pf = &f;
-  pf->printf();
+  cout << sizeof(c) << endl;
 
-  father *pc = &c;
-  pc->printf();
-
-  // 虚析构函数
-  father *nf = new child(10);
-  stage(nf);
   return 0;
 }
