4d99eaaf346b5814ccb042dbe5f676ba1264e4d2
zouzhenxing <2530177972@qq.com>
2019-08-01 22:42:52 +0800

    thread pthread_join used

---
+++ C/thread_operator.c
@@ -0,0 +1,54 @@
+#include <stdio.h>
+#include <pthread.h>
+#include <stdlib.h>
+
+/**
+ *  gcc -o thread multi_thread.c -lpthread
+ */
+ /* code */
+ // 1.创建线程号,线程标识
+
+pthread_t ntid1;
+pthread_t ntid2;
+void * retval;
+
+void thread_fun1(char *pstr)
+{
+  while(1) {
+    printf("-%s-\n", pstr);
+    sleep(1);
+  }
+}
+
+void thread_fun2(char *pstr)
+{
+  int i = 0;
+  for(i; i < 10; i++) {
+    printf("-%s-\n", pstr);
+    sleep(1);
+  }
+}
+
+int main(int argc, char const *argv[])
+{
+  char *p1 = "thread1";
+  char *p2 = "thread2";
+
+  //创建线程,成功返回0
+  int err;
+  err = pthread_create(&ntid1, NULL, (void*)&thread_fun1, p1);
+  if (err) {
+    printf("thread create error! %d", strerror(err));
+    return 0;
+  }
+
+  err = pthread_create(&ntid2, NULL, (void *)&thread_fun2, p2);
+  if (err) {
+    printf("thread create error! %d", strerror(err));
+    return 0;
+  }
+
+  pthread_join(ntid1, &retval);
+  //printf("run");
+  return 0;
+}
