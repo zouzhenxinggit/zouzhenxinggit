9b6220e80e21312486bce45bdae0bf41408c85ea
zouzhenxing <zouzhenxing.it@foxmail.com>
2019-08-29 17:56:12 +0800

    pthread rwlock

--- pthread/pthread_rwlock_init.c
+++ pthread/pthread_rwlock_init.c
@@ -1,49 +1,87 @@
-/*  Copyright (C) <2019-08-28>  <zouzhenxing>
-
-    This program is free software: you can redistribute it and/or modify
-    it under the terms of the GNU General Public License as published by
-    the Free Software Foundation, either version 3 of the License, or
-    (at your option) any later version.
-
-    This program is distributed in the hope that it will be useful,
-    but WITHOUT ANY WARRANTY; without even the implied warranty of
-    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
-    GNU General Public License for more details.
-
-    You should have received a copy of the GNU General Public License
-    along with this program.  If not, see <https://www.gnu.org/licenses/>.
-    You can also contact me by email <zouzhenxing.it@foxmail.com>.  */
+/*
+ * pthread_rwlock.c
+ *
+ * The windows message processing methods.
+ *
+ * Copyright (c) 1999-2000 Pawel W. Olszta. All Rights Reserved.
+ * Written by Pawel W. Olszta, <olszta@sourceforge.net>
+ * Creation date: Fri Dec 3 1999
+ *
+ * Permission is hereby granted, free of charge, to any person obtaining a
+ * copy of this software and associated documentation files (the "Software"),
+ * to deal in the Software without restriction, including without limitation
+ * the rights to use, copy, modify, merge, publish, distribute, sublicense,
+ * and/or sell copies of the Software, and to permit persons to whom the
+ * Software is furnished to do so, subject to the following conditions:
+ *
+ * The above copyright notice and this permission notice shall be included
+ * in all copies or substantial portions of the Software.
+ *
+ * THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS
+ * OR IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+ * FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT.  IN NO EVENT SHALL
+ * PAWEL W. OLSZTA BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER LIABILITY, WHETHER
+ * IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM, OUT OF OR IN
+ * CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE SOFTWARE.
+ */
 
 #include <stdio.h>
 #include <pthread.h>
 
+/* -- GLOBAL VARIABLES --------------------------------------------- */
 
-//pthread_rwlock_t rwlock = PTHREAD_RWLOCK_INITIALIZER;
+/*
+ * pthread variable
+ */
 pthread_rwlock_t rwlock;
+// pthread_rwlock_t rwlock = PTHREAD_RWLOCK_INITIALIZER;
 pthread_t ntid1;
 pthread_t ntid2;
 
-int rw_data;
+int rw_data = 8;
 
+/* -- PRIVATE FUNCTIONS ---------------------------------------------------- */
 
+/*
+ * write pthread
+ */
 void *pthread_write(void* arg) {
   int i;
 
   for (i = 0; i < 10; ++i) {
     sleep(1);
+
+    pthread_rwlock_wrlock(&rwlock);
+    rw_data = i;
+    pthread_rwlock_unlock(&rwlock);
+
     printf("pthread_write, tid = %d\n", (int)pthread_self());
   }
 }
 
+/*
+ * read pthread
+ */
 void *pthread_read(void* arg) {
   int i;
 
   for (i = 0; i < 10; ++i) {
-    sleep(1);
-    printf("pthread_read, tid = %d\n", (int)pthread_self());
+    sleep(2);
+
+    pthread_rwlock_rdlock(&rwlock);
+    printf("%d\n", rw_data);
+    pthread_rwlock_unlock(&rwlock);
+
+    printf("pthread_read, tid = %d, rw_data\n", (int)pthread_self());
   }
 }
 
+/*
+ * pthread_rwlock_wrlock && pthread_rwlock_rdlock
+ * read - read Only one can be read at a time
+ * write - write Only one can be write at a time
+ * read - write priority write > read
+ */
 int main(int argc, char const *argv[]) {
   int status = 0;
 
@@ -76,7 +114,6 @@ int main(int argc, char const *argv[]) {
   if (status) {
     printf("pthread_rwlock_destory: %d\n", strerror(status));
   }
-
   return 0;
 }
 
